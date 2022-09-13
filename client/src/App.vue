ccac35c2f4fb96674b44eb90f820f41625831a8c
Nabiel Alif <nabiel.alif01@gmail.com>
2022-09-13 14:17:46 +0700

    Working on chat feature

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+import ChatBox from '@/components/ChatBox.vue';
+
+export default {
+  components: {
+    ChatBox,
+  },
+};
+</script>
+
+<template>
+  <ChatBox></ChatBox>
+  <RouterView />
+</template>
+
+<style></style>
