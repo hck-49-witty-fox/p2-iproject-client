76ccd506d00aa9b2a5b36e148760f62c89441e99
ihza <ihzan0@gmail.com>
2022-09-15 07:55:30 +0700

    sign up complete components

---
+++ vue-project/src/components/SignUpComplete.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="max-w-screen-xl px-4 py-8 mx-auto">
+    <div class="py-8 bg-white dark:bg-gray-900 lg:py-0">
+      <div class="lg:flex">
+        <div
+          class="flex items-center mx-auto md:w-[42rem] px-4 md:px-8 xl:px-0"
+        >
+          <div class="w-full">
+            <div
+              class="flex items-center justify-center mb-8 space-x-4 lg:hidden"
+            >
+              <a href="#" class="flex items-center text-2xl font-semibold">
+                <img class="w-8 h-8 mr-2" src="../assets/logo.png" />
+                <span class="text-gray-900 dark:text-white">Hearify</span>
+              </a>
+            </div>
+            <ol
+              class="flex items-center mb-6 text-sm font-medium text-center text-gray-500 dark:text-gray-400 lg:mb-12 sm:text-base"
+            >
+              <li
+                class="flex items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-12 after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+              >
+                <div
+                  class="flex items-center sm:block after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500"
+                >
+                  <svg
+                    class="w-4 h-4 mr-2 sm:mb-2 sm:w-6 sm:h-6 sm:mx-auto shrink-0"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Account <span class="hidden sm:inline-flex">Info</span>
+                </div>
+              </li>
+              <li
+                class="flex items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-12 after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+              >
+                <div
+                  class="flex items-center sm:block after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500"
+                >
+                  <svg
+                    class="w-4 h-4 mr-2 sm:mb-2 sm:w-6 sm:h-6 sm:mx-auto shrink-0"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Confirmation
+                </div>
+              </li>
+              <li class="flex items-center text-blue-600 dark:text-blue-500">
+                <div class="flex items-center sm:block">
+                  <svg
+                    class="w-4 h-4 mr-2 sm:mb-2 sm:w-6 sm:h-6 sm:mx-auto shrink-0"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Payment <span class="hidden sm:inline-flex">Info</span>
+                </div>
+              </li>
+            </ol>
+            <svg
+              class="w-12 h-12 mb-4 text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <h1
+              class="mb-2 text-2xl font-extrabold tracking-tight text-gray-900 leding-tight dark:text-white"
+            >
+              Verified
+            </h1>
+            <p class="mb-4 font-light text-gray-500 dark:text-gray-400 md:mb-6">
+              You have successfully verified your account.
+            </p>
+            <router-link
+              to="/"
+              class="block w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 sm:py-3.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >Go to home page</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
