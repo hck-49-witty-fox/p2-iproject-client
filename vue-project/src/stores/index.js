133e5fdd1f8a24baa6263de26ec8a90125f80880
ihza <ihzan0@gmail.com>
2022-09-15 08:16:33 +0700

    get my playlist detail request pinia

--- vue-project/src/stores/index.js
+++ vue-project/src/stores/index.js
@@ -348,5 +348,24 @@ export const useIndexStore = defineStore("index", {
         this.loading = false;
       }
     },
+
+    async getMyPlaylistDetail(id) {
+      try {
+        this.loading = true;
+        const response = await axios.get(`${BASE_URL}/playlist/${id}`, {
+          headers: {
+            access_token: localStorage.getItem("access_token"),
+          },
+        });
+        this.myPlaylistDetail = response.data.data;
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: error.response.data.message,
+        });
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 });
