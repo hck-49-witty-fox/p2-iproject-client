1d6c2f94299212c341cbd608b9bb21fad4f45813
ihza <ihzan0@gmail.com>
2022-09-15 08:14:50 +0700

    get new release request pinia

--- vue-project/src/stores/index.js
+++ vue-project/src/stores/index.js
@@ -206,5 +206,24 @@ export const useIndexStore = defineStore("index", {
         this.loading = false;
       }
     },
+
+    async getNewRelease() {
+      try {
+        this.loading = true;
+        const response = await axios.get(`${BASE_URL}/api/get-new-release`, {
+          headers: {
+            access_token: localStorage.getItem("access_token"),
+          },
+        });
+        this.newRelease = response.data.data;
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: error.response.data.message,
+        });
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 });
