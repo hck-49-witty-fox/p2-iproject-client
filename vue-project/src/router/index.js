3ff5741f45d3aace4614e7859dfea0c85531d84b
ihza <ihzan0@gmail.com>
2022-09-15 08:04:05 +0700

    create navbar route parents

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
+import NavBar from "../components/NavBar.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "navbar",
+      component: NavBar,
+      beforeEnter: (to, from) => {
+        if (!localStorage.getItem("access_token")) {
+          return { path: "/signin" };
+        }
+      },
+      children: [],
+    },
+  ],
 });
 
 export default router;
