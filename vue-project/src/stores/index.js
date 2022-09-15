6ef783e178251f9349f7ad89b34bc441d3506f2e
ihza <ihzan0@gmail.com>
2022-09-15 08:13:32 +0700

    get featured playlist request pinia

--- vue-project/src/stores/index.js
+++ vue-project/src/stores/index.js
@@ -143,5 +143,27 @@ export const useIndexStore = defineStore("index", {
         this.loading = false;
       }
     },
+
+    async getFeaturedPlaylist() {
+      try {
+        this.loading = true;
+        const response = await axios.get(
+          `${BASE_URL}/api/get-featured-playlist`,
+          {
+            headers: {
+              access_token: localStorage.getItem("access_token"),
+            },
+          }
+        );
+        this.featuredPlaylist = response.data.data;
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: error.response.data.message,
+        });
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 });
