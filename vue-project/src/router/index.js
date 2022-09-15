28f287100ab3cb7a24b3a7f167cdfba0c4590f8e
ihza <ihzan0@gmail.com>
2022-09-15 08:05:07 +0700

    genre playlist route

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import NavBar from "../components/NavBar.vue";
 import HomePage from "../views/HomePage.vue";
 import GenrePage from "../views/GenrePage.vue";
+import GenrePlaylist from "../views/GenrePlaylist.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
           path: "genre",
           name: "genre",
           component: GenrePage,
+        },
+        {
+          path: "genre/:name",
+          name: "genreplaylist",
+          component: GenrePlaylist,
         },
       ],
     },
