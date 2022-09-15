e0799f85d780744008989b3735361a02e52855c6
ihza <ihzan0@gmail.com>
2022-09-15 08:05:30 +0700

    artist page route

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar.vue";
 import HomePage from "../views/HomePage.vue";
 import GenrePage from "../views/GenrePage.vue";
 import GenrePlaylist from "../views/GenrePlaylist.vue";
+import ArtistPage from "../views/ArtistPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,11 @@ const router = createRouter({
           path: "genre/:name",
           name: "genreplaylist",
           component: GenrePlaylist,
+        },
+        {
+          path: "artist/:id",
+          name: "artist",
+          component: ArtistPage,
         },
       ],
     },
