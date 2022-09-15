a0089fa7da2bfe3982cf330356afc2bfbab017a2
ihza <ihzan0@gmail.com>
2022-09-15 08:04:28 +0700

    homepage route

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import NavBar from "../components/NavBar.vue";
+import HomePage from "../views/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,13 @@ const router = createRouter({
           return { path: "/signin" };
         }
       },
-      children: [],
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: HomePage,
+        },
+      ],
     },
   ],
 });
